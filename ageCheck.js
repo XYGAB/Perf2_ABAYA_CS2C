98d1dcab81f05acabba4006de5cc517647a8c873
unknown <gabsslemiabaya@gmail.com>
2024-10-02 20:50:28 +0800

    Perf2 Complete

---
+++ ageCheck.js
@@ -0,0 +1,12 @@
+// If Statement
+let age = 21;
+
+console.log("Enter Your age:", age);
+//using if and else if 
+if (age>=20){
+    console.log("You are an adult.");
+} else if(age>=14){
+    console.log("You are a teenager.");
+} else {
+    console.log("You are a child.");
+}
---
+++ gradeAssignment.js
@@ -0,0 +1,14 @@
+//if else if
+let grade = 80;
+
+console.log("Enter Your grade:", grade);
+//using if else if to determine the entered grade
+if (grade>=90){
+    console.log("Excellent.");
+} else if(grade>=85){
+    console.log("Good.");
+} else if(grade>=75){
+    console.log("Fair.");
+} else {
+    console.log("Needs Improvement.");
+}
