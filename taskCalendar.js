2869b79694f6b3b724cefabd974ee0340e84165f
unknown <gabsslemiabaya@gmail.com>
2024-10-02 20:57:59 +0800

    Perf2 Complete

---
+++ taskCalendar.js
@@ -0,0 +1,24 @@
+let weekdays = "Sunday";
+
+console.log("Enter days:", weekdays);
+
+//using switch
+switch (weekdays) {
+case "Monday"://first to case
+    console.log("Lunes na naman!?");
+    break;
+case "Tuesday"://second to case
+    console.log("Katamad ka'pag second day!");
+    break;
+case "Wednesday"://third to case
+    console.log("3 days na lang");
+    break;
+case "Thursday"://fourth to case
+    console.log("Burden talaga ang day na to!!");
+    break;
+case "Friday"://fifth to case
+    console.log("Finally last day!!!");
+    break;
+default://last and it used default to stop 
+console.log("Chill muna tayo ngayong weekends.");
+}
