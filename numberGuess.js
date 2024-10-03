19dc1bce853bed3869f547a75d411950ff372c92
unknown <gabsslemiabaya@gmail.com>
2024-10-03 20:38:19 +0800

    Perf2 Complete

--- numberGuess.js
+++ numberGuess.js
@@ -1,13 +1,20 @@
-let number = 6;
-while (number <=10  && number >=4) {
-    console.log("Guess a number between 1 and 10:" + " "+ number);
-    console.log("Too high!");
-}  if (number <=2 && number >=1) {
-    console.log("Guess a number between 1 and 10:" + " " + number);
-    console.log("Too Low");
-} else if (number == 3) {
-    console.log("Guess a number between 1 and 10:" + " "+ number);
-    console.log("Correct");
-} else {
-    console.log("Try again");
+//while-loop
+const prompt = require('prompt-sync')();
+// Set the correct number
+const numberGuessed = 5; // can be change this to any number between 1 and 10
+let inputNumber = 0;
+
+// Use a while loop to keep asking until the user guesses correctly
+while (inputNumber != numberGuessed) {
+    inputNumber = parseInt(prompt("Guess a number between 1 and 10:"), 10);
+
+    if (inputNumber < numberGuessed) {
+        console.log("Too low!");
+    } else if (inputNumber > numberGuessed) {
+        console.log("Too high!");
+    } else if (inputNumber == numberGuessed) {
+        console.log("Correct!");
+    } else {
+        console.log("Please enter a valid number.");
+    }
 }
